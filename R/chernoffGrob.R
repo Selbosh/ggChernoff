cd9e4cd86f07adc7d9b0603638b4aa571b3982a4
Selby <david@selbys.org.uk>
2017-01-27 16:04:17 +0000

    Add chernoffGrob and hardcoded smiley mouth

---
+++ R/chernoffGrob.R
@@ -0,0 +1,54 @@
+#' Draw a smiley face
+#'
+#' Uses \code{\link[grid]{Grid}} graphics to draw a face.
+#'
+#' @param x horizontal position
+#' @param y vertical position
+#' @param size area of the face
+#' @param colour colour of outlines and features
+#' @param fill fill colour
+#' @param alpha transparency, where 0 is transparent and 1 is opaque
+#' @param smile amount of smiling/frowning. Currently ignored
+#' @param nose logical. Adds a nose to the face
+#'
+#' @return A \code{\link[grid]{grobTree}} object.
+#'
+#' @seealso \code{\link{geom_chernoff}}
+#'
+#' @import grid
+#' @export
+#'
+#' @examples
+#' face <- chernoffGrob(.5, .5, colour = 'navy', fill = 'coral')
+#' grid::grid.newpage()
+#' grid::grid.draw(face)
+chernoffGrob <- function(x = .5, y = .5,
+                         size = .5,
+                         colour = 'black',
+                         fill = NA,
+                         alpha = 1,
+                         smile = 1,
+                         nose = FALSE) {
+    faceGrob <- circleGrob(x, y,
+                           r = size,
+                           gp = gpar(col = colour, fill = fill)
+    )
+    eyesGrob <- circleGrob(x + rep(c(-1, +1) * size / 2.5,
+                                   each = length(x)),
+                           y + size / 10,
+                           r = size / 10,
+                           gp = gpar(col = colour, fill = colour)
+    )
+    noseGrob <- rectGrob(x, y,
+                         width = unit(size / 10, 'npc'),
+                         height = unit(size / 5, 'npc'),
+                         gp = gpar(col = NA, fill = ifelse(nose, colour, NA))
+    )
+    mouthGrob <- bezierGrob(rep(x, each = 4) + c(-1, -.5, .5, 1) * rep(size, each = 4) / 3,
+                            rep(y, each = 4) + c(-1, -2, -2, -1) * rep(size, each = 4) / 3,
+                            arrow = arrow(length = unit(0.005, 'npc'), type = 'closed'),
+                            gp = gpar(col = colour, lwd = 1),
+                            id.lengths = rep(4, length(x))
+    )
+    grobTree(faceGrob, noseGrob, eyesGrob, mouthGrob)
+}
--- R/geom_chernoff.R
+++ R/geom_chernoff.R
@@ -1,7 +1,7 @@
 #' Chernoff faces in ggplot2
 #'
-#' The Chernoff geom is used to create Chernoff-like visualisations in the shape of human faces.
-#' By mapping to the relevant aesthetics, faces can appear to vary in happiness, anger, size and so on.
+#' The Chernoff geom is used to create data visualisations in the shape of human-like faces.
+#' By mapping to the relevant aesthetics, faces can appear to vary in happiness, anger, size, colour and so on.
 #'
 #' @param mapping Set of aesthetic mappings created by \code{\link[ggplot2]{aes}} or
 #'   \code{\link[ggplot2]{aes_}}. If specified and \code{inherit.aes = TRUE} (the
@@ -21,8 +21,10 @@
 #' }
 #' The following aesthetics are unique to \code{geom_chernoff}:
 #' \itemize{
-#' \item \code{nose}: logical. Draw a nose or not? Default is \code{FALSE}. Currently cannot be customised further.
+#' \item \code{smile}
+#' \item \code{nose}
 #' }
+#' For details, see \code{\link{chernoffGrob}}.
 #'
 #' @examples
 #' library(ggplot2)
@@ -42,49 +44,27 @@
 #' The use of faces to represent points in \emph{k}-dimensional space graphically.
 #' \emph{Journal of the American Statistical Association, 68}(342), 361–368.
 #'
-#' @importFrom ggplot2 ggproto
-#' @importFrom grid circleGrob rectGrob grobTree
+#' @seealso \code{\link{chernoffGrob}}
+#'
+#' @import ggplot2
 #' @export geom_chernoff
 geom_chernoff <- function(mapping = NULL, data = NULL, stat = "identity",
                           position = "identity", na.rm = FALSE, show.legend = NA,
                           inherit.aes = TRUE, ...) {
-  ggplot2::layer(
-                 geom = GeomChernoff, mapping = mapping,  data = data, stat = stat,
-                 position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-                 params = list(na.rm = na.rm, ...)
-                )
+  layer(geom = GeomChernoff, mapping = mapping,  data = data, stat = stat,
+        position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+        params = list(na.rm = na.rm, ...)
+        )
 }
 
-GeomChernoff <- ggplot2::ggproto("GeomChernoff", ggplot2::Geom,
+GeomChernoff <- ggproto("GeomChernoff", ggplot2::Geom,
   required_aes = c("x", "y"),
-  default_aes = ggplot2::aes(colour = "black", fill = NA, size = 1, alpha = 1, nose = FALSE),
+  default_aes = aes(colour = "black", fill = NA, size = 1, alpha = 1, smile = 1, nose = FALSE),
   draw_key = ggplot2::draw_key_rect,
   draw_panel = function(data, panel_scales, coord) {
       coords <- coord$transform(data, panel_scales)
-      face <- grid::circleGrob(coords$x,
-                               coords$y,
-                               r = sqrt(coords$size) / 30,
-                               gp = grid::gpar(col = coords$colour,
-                                               fill = coords$fill)
-                               )
-      lefteye <- grid::circleGrob(coords$x - sqrt(coords$size) / 75,
-                                  coords$y + sqrt(coords$size) / 150,
-                                  r = sqrt(coords$size) / 300,
-                                  gp = grid::gpar(col = coords$colour,
-                                                  fill = coords$colour)
-                                  )
-      righteye <- grid::circleGrob(coords$x + sqrt(coords$size) / 75,
-                                   coords$y + sqrt(coords$size) / 150,
-                                   r = sqrt(coords$size) / 300,
-                                   gp = grid::gpar(col = coords$colour,
-                                                   fill = coords$colour)
-                                   )
-      nose <- grid::rectGrob(coords$x, coords$y,
-                             width = sqrt(coords$size) * unit(0.002, 'npc'),
-                             height = sqrt(coords$size) * unit(0.01, 'npc'),
-                             gp = grid::gpar(col = NA,
-                                             fill = ifelse(coords$nose, coords$colour, NA))
-                             )
-      grid::grobTree(face, nose, lefteye, righteye)
+      with(coords,
+           chernoffGrob(x, y, sqrt(size) / 35, colour, fill, alpha, smile, nose)
+           )
   }
 )
